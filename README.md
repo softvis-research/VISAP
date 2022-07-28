a7b2e945167956ef18d572a1cfe159200f59c8e3
ZZzzaaKK <66885975+ZZzzaaKK@users.noreply.github.com>
2022-07-28 18:06:26 +0200

    Start recreation of LoaderStep, update README
    An initial draft of the recreated LoaderStep from Getaviz that still contains errors. Also updates the README.md to encourage contribution if the project goes public.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # VISAP
-Visual SAP Analytics Process
+This project aims to be a revamp of https://github.com/softvis-research/Getaviz. In particular, it aims to reduce bloat in its source code while merging a lot of the features of Getaviz that were previously built separately through the efforts of many individual contributors.
+
+## Current State
+The project is currently in a pre-development phase. Porting of functionality from Getaviz is still ongoing, and while we have a rough idea of where we want to go, many important design decisions are yet to be made.
+
+## Installation and Documentation
+The development team actively uses and maintains https://miro.com/app/board/uXjVOGFnA-M=/ for project coordination and documentation. The Miro board also includes instructions for installation and usage. Access requests will generally be accepted. Most documentation is currently available in German only. If you need help or have any suggestions, we appreciate you, and hope you will file an issue or message the contributors directly.
---
+++ generator/src/main/java/org/getaviz/generator/steps/LoaderStep.java
@@ -0,0 +1,190 @@
+package org.getaviz.generator.steps;
+
+import org.getaviz.generator.abap.enums.SAPRelationLabels;
+import org.getaviz.generator.database.DatabaseConnector;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class LoaderStep {
+    private static final DatabaseConnector connector = DatabaseConnector.getInstance("bolt://localhost:11020");
+    public static void main(String[] args) {
+        boolean isSilentMode = true;
+        String pathToNodesCsv = "";
+        String pathToReferenceCsv = "";
+        String pathToInheritanceCsv = "";
+        String pathToMigrationFindingsCsv = "";
+        String pathToMigrationFindingsNewCsv = "";
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("Silent mode? (y/n): "); // Silent mode to run with default values
+        String input = userInput.nextLine();
+        if (input.equals("n")) {
+            isSilentMode = false;
+        }
+
+        // Get files for nodes and relations
+        List<Path> files = CSVInput.getInputCSVFiles();
+        for(Path p : files) {
+            if (p.toString().endsWith("_Test.csv")) {
+                pathToNodesCsv = p.toString();
+            } else if (p.toString().endsWith("_Reference.csv")) {
+                pathToReferenceCsv = p.toString();
+            } else if (p.toString().endsWith("_Inheritance.csv")) {
+                pathToInheritanceCsv = p.toString();
+                //} else if (p.toString().endsWith("_export.csv")) {
+                //  pathToMigrationFindingsCsv = p.toString();
+            } else if (p.toString().endsWith("_rc3.0.csv")) {
+                pathToMigrationFindingsNewCsv = p.toString();
+            }
+        }
+
+        if (pathToNodesCsv.isEmpty() || pathToInheritanceCsv.isEmpty() || pathToReferenceCsv.isEmpty())
+                // should config stay the same?
+               // || (config.addMigrationFindings() && pathToMigrationFindingsNewCsv.isEmpty()))
+        {
+            System.out.println("Some input file wasn't found");
+            System.exit(0);
+        }
+
+
+        // Make sure the graph is empty
+        connector.executeWrite("MATCH (n) DETACH DELETE n;");
+
+        // 1. Upload nodes
+        System.out.println("SAPExportCreateNodes: " + pathToNodesCsv);
+        if (!isSilentMode) {
+            System.out.print("Loading nodes in Neo4j. Press any key to continue...");
+            userInput.nextLine();
+        }
+        pathToNodesCsv = pathToNodesCsv.replace("\\", "/");
+        connector.executeWrite(
+                "LOAD CSV WITH HEADERS FROM \"file:///" + pathToNodesCsv + "\"\n" +
+                        "AS row FIELDTERMINATOR ';'\n" +
+                        "CREATE (n:Elements)\n" +
+                        "SET n = row"
+        );
+
+
+        // 2. Upload contains relations
+        if (!isSilentMode) {
+            System.out.print("Creating 'CONTAINS' relationships. Press any key to continue...");
+            userInput.nextLine();
+        }
+        connector.executeWrite("MATCH (a:Elements), (b:Elements) " +
+                "WHERE a.element_id = b.container_id " +
+                "CREATE (a)-[r:" + SAPRelationLabels.CONTAINS + "]->(b)"
+        );
+
+        // 3. Upload uses relations
+        if (!isSilentMode) {
+            System.out.print("Creating 'USES' relationships. Press any key to continue...");
+            userInput.nextLine();
+        }
+        connector.executeWrite("MATCH (a:Elements), (b:Elements) " +
+                "WHERE a.element_id = b.uses_id " +
+                "CREATE (a)-[r:" + SAPRelationLabels.USES + "]->(b)"
+        );
+
+        // 4. Upload References
+        System.out.println("Path to Reference CSV: " + pathToReferenceCsv);
+        if (!isSilentMode) {
+            System.out.print("Creating 'REFERENCE' relationships. Press any key to continue...");
+            userInput.nextLine();
+        }
+        pathToReferenceCsv = pathToReferenceCsv.replace("\\", "/");
+        connector.executeWrite(
+                "LOAD CSV WITH HEADERS FROM \"file:///" + pathToReferenceCsv + "\"\n" +
+                        "AS row FIELDTERMINATOR ';'\n" +
+                        "MATCH (a:Elements {element_id: row.source_id}), (b:Elements {element_id: row.target_id})\n" +
+                        "CREATE (a)-[r:"+ SAPRelationLabels.REFERENCES +"]->(b)"
+
+        );
+
+        // 5. Upload Inheritances
+        System.out.println("Path to Inheritances CSV: " + pathToInheritanceCsv);
+        if (!isSilentMode) {
+            System.out.print("Creating 'INHERIT' relationships. Press any key to continue...");
+            userInput.nextLine();
+        }
+        pathToInheritanceCsv = pathToInheritanceCsv.replace("\\", "/");
+        connector.executeWrite(
+                "LOAD CSV WITH HEADERS FROM \"file:///" + pathToInheritanceCsv + "\"\n" +
+                        "AS row FIELDTERMINATOR ';'\n" +
+                        "MATCH (a:Elements {element_id: row.subclass_id}), (b:Elements {element_id: row.superclass_id})\n" +
+                        "CREATE (a)-[r:"+ SAPRelationLabels.INHERIT +"]->(b)"
+        );
+
+        // 6. Upload Migration Findings NEW - for districts and buildings
+        if(config.addMigrationFindings()) {
+            System.out.println("Path to Migration Findings CSV : " + pathToMigrationFindingsNewCsv);
+            if (!isSilentMode) {
+                System.out.print("ADDING 'MIGRATION_FINDINGS' to Element-Nodes. Press any key to continue...");
+                userInput.nextLine();
+            }
+            pathToMigrationFindingsNewCsv = pathToMigrationFindingsNewCsv.replace("\\", "/");
+            connector.executeWrite(
+                    "LOAD CSV WITH HEADERS FROM \"file:///" + pathToMigrationFindingsNewCsv + "\"\n" +
+                            "AS row FIELDTERMINATOR ';'\n" +
+                            "MATCH (a:Elements {object_name: row.SUB_OBJ_NAME})\n" +
+                            "SET a.migration_findings = \"true\"\n"
+            );
+            connector.executeWrite(
+                    "LOAD CSV WITH HEADERS FROM \"file:///" + pathToMigrationFindingsNewCsv + "\"\n" +
+                            "AS row FIELDTERMINATOR ';'\n" +
+                            "MATCH (a:Elements {object_name: row.OBJ_NAME})\n" +
+                            "SET a.migration_findings = \"true\"\n"
+
+            );
+        }
+
+        /*// 6. Upload Migration Findings - only for districts
+        if(config.addMigrationFindings()) {
+            System.out.println("Path to Migration Findings UE CSV : " + pathToMigrationFindingsCsv);
+            if (!isSilentMode) {
+                System.out.print("ADDING 'MIGRATION_FINDINGS' to Element-Nodes. Press any key to continue...");
+                userInput.nextLine();
+            }
+            pathToMigrationFindingsCsv = pathToMigrationFindingsCsv.replace("\\", "/");
+            connector.executeWrite(
+                    "LOAD CSV WITH HEADERS FROM \"file:///" + pathToMigrationFindingsCsv + "\"\n" +
+                            "AS row FIELDTERMINATOR ';'\n" +
+                            "MATCH (a:Elements {object_name: row.Objektname})\n" +
+                            "SET a.migration_findings = \"true\""
+            );
+        }*/
+
+        connector.close();
+        System.out.println("Loader step was completed");
+    }
+
+    private static class CSVInput {
+        private static List<Path> getInputCSVFiles() {
+            String path = config.getString("input.map", "src/test/neo4jexport/");
+            File currentDir = new File(path);
+            String helper = currentDir.getAbsolutePath();
+            List<Path> files = new ArrayList<>();
+            try {
+                files = Files.walk(Paths.get(helper), 1)
+                        .filter(Files::isRegularFile)
+                        .collect(Collectors.toList());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return files;
+        }
+
+        private static String getFilepath() {
+
+        }
+    }
+
+
+}
