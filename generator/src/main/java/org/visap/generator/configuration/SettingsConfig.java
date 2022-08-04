aab7e82afa6ef3a523c4e4db624fb9e11f49fa41
ZZzzaaKK <66885975+ZZzzaaKK@users.noreply.github.com>
2022-08-04 15:23:54 +0200

    Implement config concept
    Use the OWNER library to implement a concept to allow for easy reading of configuration parameters

--- generator/src/main/java/org/visap/generator/configuration/SettingsConfig.java
+++ generator/src/main/java/org/visap/generator/configuration/SettingsConfig.java
@@ -2,6 +2,9 @@ package org.visap.generator.configuration;
 
 import org.aeonbits.owner.Config;
 
+@Config.Sources(value = "file:${user.dir}/src/main/java/org/visap/generator/configuration/SettingsConfig.properties")
 public interface SettingsConfig extends Config {
     String boltAddress();
+    @DefaultValue("5")
+    int maxThreads();
 }
--- generator/src/main/java/org/visap/generator/test/LoaderStepTest.java
+++ generator/src/main/java/org/visap/generator/test/LoaderStepTest.java
@@ -1,23 +1,25 @@
 package org.visap.generator.test;
 
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.visap.generator.configuration.SettingsConfig;
 
 class LoaderStepTest {
+    SettingsConfig config = ConfigFactory.create(SettingsConfig.class);
+
     @Test
-    void OneEqualsOne() {
-        assertEquals(1, 1);
-    }
-    @Test
-    void TwoDoesNotEqualOne() {
-        assertNotEquals(1, 2);
+    void settingsConfigIsInstantiated() {
+        assertNotNull(config);
     }
     @Test
     void boltAddressIsNotEmpty() {
-        SettingsConfig config = ConfigFactory.create(SettingsConfig.class);
-        assertNotEquals(config.boltAddress(), null);
+        assertNotNull(config.boltAddress());
+    }
+    @Test
+    void maxThreadsIs5() {
+        assertEquals(5, config.maxThreads());
     }
 }
