d1ae8e30582144ad80de5e431d19d2361aecc1c0
ZZzzaaKK <66885975+ZZzzaaKK@users.noreply.github.com>
2022-08-19 13:23:40 +0200

    Begin implementation of LayouterStep

---
+++ generator/src/main/java/org/visap/generator/layouts/ABuildingLayout.java
@@ -0,0 +1,179 @@
+package org.visap.generator.layouts;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.visap.generator.configuration.SettingsConfig;
+import org.visap.generator.abap.enums.SAPNodeProperties;
+import org.visap.generator.abap.enums.SAPNodeTypes;
+import org.visap.generator.layouts.ABuildingSegmentLayout;
+import org.visap.generator.repository.ACityElement;
+
+import java.util.Collection;
+
+public class ABuildingLayout {
+
+    private Log log = LogFactory.getLog(this.getClass());
+    private SettingsConfig config;
+
+    private ACityElement building;
+    private Collection<ACityElement> floors;
+    private Collection<ACityElement> chimneys;
+
+    public ABuildingLayout(ACityElement building, Collection<ACityElement> floors, Collection<ACityElement> chimneys, SettingsConfig config) {
+        this.config = config;
+
+        this.building = building;
+        this.floors = floors;
+        this.chimneys = chimneys;
+    }
+
+
+
+    public void calculate(){
+
+        ABuildingSegmentLayout buildingSegmentLayout = new ABuildingSegmentLayout(building, floors, chimneys, config);
+        buildingSegmentLayout.calculate();
+
+        setSizeOfBuilding();
+        setPositionOfBuilding();
+
+    }
+
+    private void setPositionOfBuilding() {
+
+        building.setXPosition(0.0);
+        building.setYPosition(building.getHeight() / 2);
+        building.setZPosition(0.0);
+
+    }
+
+
+    private void setSizeOfBuilding() {
+
+        ACityElement.ACitySubType referenceBuildingType = building.getSubType();
+
+        if(referenceBuildingType != null) {
+            switch (referenceBuildingType) {
+                case Sea:
+                    building.setHeight(config.getMetropolisReferenceBuildingHeigth("seaReferenceBuilding"));
+                    building.setWidth(config.getMetropolisReferenceBuildingWidth("seaReferenceBuilding"));
+                    building.setLength(config.getMetropolisReferenceBuildingLength("seaReferenceBuilding"));
+                    break;
+
+                case Mountain:
+                    building.setHeight(config.getMetropolisReferenceBuildingHeigth("mountainReferenceBuilding"));
+                    building.setWidth(config.getMetropolisReferenceBuildingWidth("mountainReferenceBuilding"));
+                    building.setLength(config.getMetropolisReferenceBuildingLength("mountainReferenceBuilding"));
+                    break;
+
+                case Cloud:
+                    building.setHeight(config.getMetropolisReferenceBuildingHeigth("cloudReferenceBuilding"));
+                    building.setWidth(config.getMetropolisReferenceBuildingWidth("cloudReferenceBuilding"));
+                    building.setLength(config.getMetropolisReferenceBuildingLength("cloudReferenceBuilding"));
+                    break;
+            }
+        } else {
+            Double floorHeightSum = calculateFloorHeightSum();
+            Double biggestChimneyHeight = getBiggestChimneyHeight();
+            Double groundAreaLength = calculateGroundAreaByChimneyAmount();
+
+            building.setWidth(groundAreaLength);
+            building.setLength(groundAreaLength);
+            building.setHeight(floorHeightSum);
+        }
+    }
+
+    private Double getBiggestChimneyHeight() {
+        double biggestChimneyHeight = 0.0;
+        for(ACityElement chimney : chimneys){
+            double chimneyHeight = chimney.getHeight();
+            if(chimneyHeight > biggestChimneyHeight){
+                biggestChimneyHeight = chimneyHeight;
+            }
+        }
+        return biggestChimneyHeight;
+    }
+
+    private Double calculateFloorHeightSum() {
+        // no floors & no numberOfStatements => default
+        double floorHeightSum = config.getFloorHeightSum();
+
+        //no floors, but numberOfStatements
+        if (floors.isEmpty()){
+            if(building.getSourceNode() == null){
+                return floorHeightSum;
+            }
+            String NOS = building.getSourceNodeProperty(SAPNodeProperties.number_of_statements);
+
+            if(NOS != "null") {
+                Double nos = Double.valueOf(NOS);
+
+                //numberOfStatements = 0 --> concerns SAP Standard objects
+                if (nos == 0){
+                    String iterationString = building.getSourceNodeProperty(SAPNodeProperties.iteration);
+                    int iteration = Integer.parseInt(iterationString);
+                    String type_name = building.getSourceNodeProperty(SAPNodeProperties.type_name);
+
+                    if(iteration >= 0) {
+                        if (building.getSourceNodeProperty(SAPNodeProperties.creator).equals("SAP")) {
+                            if (!type_name.equals(SAPNodeTypes.Attribute)) {
+                                floorHeightSum = config.getAbapStandardCodeHeight();
+                            }
+                        }
+                    }
+                } else {
+                    floorHeightSum = getScaledHeightNew(nos);
+                }
+            }
+        }
+
+        //floors, but no numberOfStatements
+        for (ACityElement floor : floors) {
+            double floorTopEdge = floor.getYPosition() + (floor.getHeight() / 2);
+            if (floorTopEdge > floorHeightSum) {
+                floorHeightSum = floorTopEdge;
+            }
+        }
+
+        return floorHeightSum;
+    }
+
+    private double getScaledHeight(double unscaledHeight) {
+        if (unscaledHeight < config.getAbapScoMinHeight()) {
+            return config.getAbapScoMinHeight();
+        } else if (unscaledHeight > config.getAbapScoMaxHeight()) {
+            return config.getAbapScoMaxHeight();
+        } else {
+            return unscaledHeight;
+        }
+    }
+
+    private double getScaledHeightNew(double unscaledHeight) {
+
+        unscaledHeight = unscaledHeight / config.getACityFactorMaxHeight();
+
+        if (unscaledHeight < config.getAbapScoMinHeight()) {
+            return config.getAbapScoMinHeight();
+        } else if (unscaledHeight > config.getAbapScoMaxHeight()) {
+            return config.getAbapScoMaxHeight();
+        } else {
+            return unscaledHeight;
+        }
+    }
+
+    private double calculateGroundAreaByChimneyAmount() {
+        if (chimneys.size() < 2){
+            return config.getACityGroundAreaByChimneyAmount();
+        }
+
+        int chimneyAmount = chimneys.size();
+        double chimneySurface = Math.sqrt(chimneyAmount);
+
+        return Math.ceil(chimneySurface);
+    }
+
+
+
+
+}
+
