27655dd4491085e0378ea2e730dfe5c8246e9bb6
ZZzzaaKK <66885975+ZZzzaaKK@users.noreply.github.com>
2022-08-19 12:58:14 +0200

    Change config for better structuring

--- generator/src/main/java/org/visap/generator/configuration/SettingsConfig.java
+++ generator/src/main/java/org/visap/generator/configuration/SettingsConfig.java
@@ -2,7 +2,11 @@ package org.visap.generator.configuration;
 
 import org.aeonbits.owner.Config;
 
-@Config.Sources(value = "file:${user.dir}/src/main/java/properties/SettingsConfig.properties")
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({
+        "file:${user.dir}/src/main/java/properties/SetupConfig.properties",
+        "file:${user.dir}/src/main/java/properties/VisualizationConfig.properties"
+})
 public interface SettingsConfig extends Config {
     @Key("setup.boltAddress")
     String boltAddress();
@@ -11,10 +15,13 @@ public interface SettingsConfig extends Config {
     @Key("setup.silentMode")
     @DefaultValue("true")
     boolean silentMode();
+    @Key("visualization.showMountainReferenceBuilding")
     @DefaultValue("false")
     boolean showMountainReferenceBuilding();
+    @Key("visualization.showSeaReferenceBuilding")
     @DefaultValue("false")
     boolean showSeaReferenceBuilding();
+    @Key("visualization.showCloudReferenceBuilding")
     @DefaultValue("false")
     boolean showCloudReferenceBuilding();
 }
