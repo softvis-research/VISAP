cadb4e18324fc788a16092a5922514bd488ba4b4
asgdf <asgdf@users.noreply.github.com>
2023-02-02 17:36:07 +0100

    Remove YourOutput from repo, instead auto-create dir when needed

--- generator/src/main/java/org/visap/generator/metaphors/metropolis/steps/MetaDataExporter.java
+++ generator/src/main/java/org/visap/generator/metaphors/metropolis/steps/MetaDataExporter.java
@@ -14,6 +14,9 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.*;
@@ -30,9 +33,9 @@ public class MetaDataExporter {
     public void exportMetaDataFile() {
         Writer fw = null;
         try {
-            File outputDir = new File(Config.output.mapPath());
-            String path = outputDir.getAbsolutePath() + "/metaData.json";
-            fw = new FileWriter(path);
+            Path outputDir = Files.createDirectories(Paths.get(Config.output.mapPath()));
+            Path metadataPath = outputDir.resolve("metaData.json").toAbsolutePath();
+            fw = new FileWriter(metadataPath.toString());
             fw.write(toJSON(cityRepository.getAllElements()));
         } catch (IOException e) {
             System.out.println(e);
