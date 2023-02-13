48c972c08c5946cf7d8fc5d175a310d4dfdab1b4
ZZzzaaKK <randufresne@proton.me>
2023-02-13 16:47:33 +0100

    Reproduce error DomHelper.js:28 Uncaught (in promise) TypeError: Cannot read properties of undefined (reading 'appendChild')     at DomHelper.buildUiHead (DomHelper.js:28:22)     at Object.activate (MetricController.js:46:19)     at activateController (Application.js:238:21)     at Application.js:73:4     at Array.forEach (<anonymous>)     at Object.initialize (Application.js:70:21)     at initializeApplication (Application.js:32:14)

---
+++ ui/setups/metrics.js
@@ -0,0 +1,51 @@
+const setup = {
+    controllers: [
+        {
+            name: "defaultLogger",
+
+            logActionConsole: false,
+            logEventConsole: false
+        },
+        {
+            name: "canvasHoverController",
+        },
+        {
+            name: "metricController",
+        },
+    ],
+
+    ui: {
+        name: "UI0",
+
+        navigation: {
+            //examine, walk, fly, helicopter, lookAt, turntable, game
+            type: "examine",
+            //speed: 10
+        },
+        area: {
+            name: "top",
+            orientation: "horizontal",
+            first: {
+                size: "80%",
+                collapsible: false,
+                name: "canvas",
+                canvas: {},
+
+                controllers: [
+                    { name: "defaultLogger" },
+                    { name: "canvasHoverController" },
+                ],
+            },
+            second: {
+                size: "20%",
+                collapsible: false,
+                name: "metrics",
+                metrics: {},
+
+                controllers: [
+                    { name: "metricController" },
+                ],
+            }
+        }
+    }
+};
