4c64f014b6c63ebe45e45011fa5863242607cb5f
asgdf <asgdf@users.noreply.github.com>
2023-07-05 10:40:31 +0200

    Simplify equality comparison

--- ui/scripts/Metric/MetricController.js
+++ ui/scripts/Metric/MetricController.js
@@ -250,19 +250,12 @@ controllers.metricController = (function () {
 	}
 
 	function isEqual(obj1, obj2) {
-		const props1 = Object.getOwnPropertyNames(obj1);
-		const props2 = Object.getOwnPropertyNames(obj2);
-		if (props1.length != props2.length) {
-			return false;
-		}
-		for (let i = 0; i < props1.length; i++) {
-			const [val1, val2] = (obj1[props1[i]], obj2[props1[i]]);
-			const areObjects = isObject(val1) && isObject(val2);
-			if (areObjects && !isEqual(val1, val2) || !areObjects && val1 !== val2) {
-				return false;
-			}
-		}
-		return true;
+		if (isObject(obj1) && isObject(obj2)) {
+			return Object.keys(obj1).length === Object.keys(obj2).length &&
+				Object.keys(obj1).every(key => obj2.hasOwnProperty(key) && isEqual(obj1[key], obj2[key]));
+		 } else {
+			return obj1 === obj2;
+		 }
 	}
 
 	function isObject(object) {
