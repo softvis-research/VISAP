28cdfa3b5528ae36b4653e8688349369c8aaf542
ZZzzaaKK <66885975+ZZzzaaKK@users.noreply.github.com>
2023-07-24 14:05:24 +0200

    Create file to specify config locations

---
+++ generator/src/main/java/org/visap/generator/configuration/Sources.java
@@ -0,0 +1,6 @@
+package org.visap.generator.configuration;
+
+public class Sources {
+	public static final String SETUP_LOCAL_PATH = "file:${user.dir}/properties/local/";
+	public static final String SETUP_PATH = "file:${user.dir}/properties/master/";
+}
