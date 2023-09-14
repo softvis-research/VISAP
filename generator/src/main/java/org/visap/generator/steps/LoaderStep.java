4080816fcc475939e335cc441f2f051e34659c67
Louay <L.kurhabi@htwk.de>
2023-09-14 17:01:00 +0200

    update LodaerStep 1. Advanced search for node types. 2. additional attributes added.

--- generator/src/main/java/org/visap/generator/steps/LoaderStep.java
+++ generator/src/main/java/org/visap/generator/steps/LoaderStep.java
@@ -84,19 +84,19 @@ public class LoaderStep {
         //2.1 Between main elements and sub elements
         connector.executeWrite("MATCH (n:Elements) WHERE n.SUB_OBJ_NAME IS NOT NULL AND n.SUB_SUB_OBJ_NAME IS NULL\n" +
                 "UNWIND n AS sub_element\n" +
-                "MATCH (p:Elements {MAIN_OBJ_NAME: sub_element.MAIN_OBJ_NAME}) WHERE p.SUB_OBJ_NAME IS NULL AND p.SUB_SUB_OBJ_NAME IS NULL\n"+
+                "MATCH (p:Elements {MAIN_OBJ_NAME: sub_element.MAIN_OBJ_NAME, MAIN_OBJ_TYPE: sub_element.MAIN_OBJ_TYPE }) WHERE p.SUB_OBJ_NAME IS NULL AND p.SUB_SUB_OBJ_NAME IS NULL\n"+
                 "CREATE (p)-[r:" + SAPRelationLabels.CONTAINS + "]->(sub_element)");
 
         //2.2 Between sub elements ans sub sub elements
         connector.executeWrite("MATCH (n:Elements) WHERE n.SUB_SUB_OBJ_NAME IS NOT NULL\n" +
                 "UNWIND n AS sub_element\n" +
-                "MATCH (p:Elements {MAIN_OBJ_NAME: sub_element.MAIN_OBJ_NAME ,SUB_OBJ_NAME: sub_element.SUB_OBJ_NAME}) WHERE p.SUB_SUB_OBJ_NAME IS NULL\n"+
+                "MATCH (p:Elements {MAIN_OBJ_NAME: sub_element.MAIN_OBJ_NAME, MAIN_OBJ_TYPE: sub_element.MAIN_OBJ_TYPE ,SUB_OBJ_NAME: sub_element.SUB_OBJ_NAME, SUB_OBJ_TYPE: sub_element.SUB_OBJ_TYPE}) WHERE p.SUB_SUB_OBJ_NAME IS NULL\n"+
                 "CREATE (p)-[r:" + SAPRelationLabels.CONTAINS + "]->(sub_element)");
 
         //2.3 Between main elements and Packages
         connector.executeWrite("MATCH (n:Elements) WHERE n.SUB_SUB_OBJ_NAME IS NULL AND n.SUB_OBJ_NAME IS NULL\n" +
                 "UNWIND n AS main_element\n" +
-                "MATCH (p:Elements {MAIN_OBJ_NAME: main_element.PACKAGE}) WHERE p.PACKAGE IS NULL\n"+
+                "MATCH (p:Elements {MAIN_OBJ_NAME: main_element.PACKAGE, MAIN_OBJ_TYPE: 'DEVC'}) WHERE p.MAIN_OBJ_NAME <> main_element.MAIN_OBJ_NAME\n"+
                 "CREATE (p)-[r:" + SAPRelationLabels.CONTAINS + "]->(main_element)");
 
         // 3. Upload Meta
@@ -110,11 +110,11 @@ public class LoaderStep {
         connector.executeWrite(
                 "LOAD CSV WITH HEADERS FROM \"file:///" + pathToMetasCsv + "\"\n" +
                         "AS row FIELDTERMINATOR ';' WITH row WHERE row.MAIN_OBJ_NAME IS NOT NULL\n" +
-                        "OPTIONAL MATCH (m:Elements {MAIN_OBJ_NAME : row.MAIN_OBJ_NAME})\n" +
+                        "OPTIONAL MATCH (m:Elements {MAIN_OBJ_NAME : row.MAIN_OBJ_NAME, MAIN_OBJ_TYPE : row.MAIN_OBJ_TYPE})\n" +
                         "WHERE m.SUB_OBJ_NAME IS NULL AND  row.SUB_OBJ_NAME IS NULL\n" +
-                        "OPTIONAL MATCH (s:Elements {MAIN_OBJ_NAME : row.MAIN_OBJ_NAME, SUB_OBJ_NAME: row.SUB_OBJ_NAME})\n" +
+                        "OPTIONAL MATCH (s:Elements {MAIN_OBJ_NAME : row.MAIN_OBJ_NAME, MAIN_OBJ_TYPE : row.MAIN_OBJ_TYPE, SUB_OBJ_NAME: row.SUB_OBJ_NAME, SUB_OBJ_TYPE: row.SUB_OBJ_TYPE})\n" +
                         "WHERE s.SUB_SUB_OBJ_NAME IS NULL AND row.SUB_SUB_OBJ_NAME IS NULL\n"+
-                        "OPTIONAL MATCH (ss:Elements {MAIN_OBJ_NAME : row.MAIN_OBJ_NAME, SUB_OBJ_NAME: row.SUB_OBJ_NAME, SUB_SUB_OBJ_NAME: row.SUB_SUB_OBJ_NAME})\n" +
+                        "OPTIONAL MATCH (ss:Elements {MAIN_OBJ_NAME : row.MAIN_OBJ_NAME, MAIN_OBJ_TYPE : row.MAIN_OBJ_TYPE, SUB_OBJ_NAME: row.SUB_OBJ_NAME, SUB_OBJ_TYPE: row.SUB_OBJ_TYPE, SUB_SUB_OBJ_NAME: row.SUB_SUB_OBJ_NAME, SUB_SUB_OBJ_TYPE: row.SUB_SUB_OBJ_TYPE})\n" +
                         "WHERE row.SUB_SUB_OBJ_NAME IS NOT NULL AND row.SUB_OBJ_NAME IS NOT NULL\n"+
                         "SET m.creator = row.CREATOR , m.created = row.CREATED , m.progname = row.PROGNAME, m.changed_by = row.CHANGED_BY, m.changed = row.CHANGED\n" +
                         "SET s.creator = row.CREATOR , s.created = row.CREATED , s.progname = row.PROGNAME, s.changed_by = row.CHANGED_BY, s.changed = row.CHANGED\n" +
@@ -134,21 +134,21 @@ public class LoaderStep {
                 "LOAD CSV WITH HEADERS FROM \"file:///" + pathToReferenceCsv + "\"\n" +
                         "AS row FIELDTERMINATOR ';' WITH row WHERE row.MAIN_OBJ_NAME_SRC IS NOT NULL\n" +
 
-                        "OPTIONAL MATCH (md:Elements {MAIN_OBJ_NAME : row.MAIN_OBJ_NAME_DST})\n" +
+                        "OPTIONAL MATCH (md:Elements {MAIN_OBJ_NAME : row.MAIN_OBJ_NAME_DST, MAIN_OBJ_TYPE: row.MAIN_OBJ_TYPE_DST})\n" +
                         "WHERE md.SUB_OBJ_NAME IS NULL AND row.SUB_OBJ_NAME_DST IS NULL \n" +
-                        "OPTIONAL MATCH (sd:Elements {MAIN_OBJ_NAME : row.MAIN_OBJ_NAME_DST, SUB_OBJ_NAME: row.SUB_OBJ_NAME_DST})\n" +
+                        "OPTIONAL MATCH (sd:Elements {MAIN_OBJ_NAME : row.MAIN_OBJ_NAME_DST, MAIN_OBJ_TYPE: row.MAIN_OBJ_TYPE_DST, SUB_OBJ_NAME: row.SUB_OBJ_NAME_DST, SUB_OBJ_TYPE: row.SUB_OBJ_TYPE_DST})\n" +
                         "WHERE sd.SUB_SUB_OBJ_NAME IS NULL AND  row.SUB_SUB_OBJ_NAME_DST IS NULL \n" +
-                        "OPTIONAL MATCH (ssd:Elements {MAIN_OBJ_NAME : row.MAIN_OBJ_NAME_DST, SUB_OBJ_NAME: row.SUB_OBJ_NAME_DST, SUB_SUB_OBJ_NAME: row.SUB_SUB_OBJ_NAME_DST})\n" +
+                        "OPTIONAL MATCH (ssd:Elements {MAIN_OBJ_NAME : row.MAIN_OBJ_NAME_DST, MAIN_OBJ_TYPE: row.MAIN_OBJ_TYPE_DST, SUB_OBJ_NAME: row.SUB_OBJ_NAME_DST, SUB_OBJ_TYPE: row.SUB_OBJ_TYPE_DST, SUB_SUB_OBJ_NAME: row.SUB_SUB_OBJ_NAME_DST, SUB_SUB_OBJ_TYPE: row.SUB_SUB_OBJ_TYPE_DST})\n" +
                         "WHERE row.SUB_SUB_OBJ_NAME_DST  IS NOT NULL AND row.SUB_OBJ_NAME_DST IS NOT NULL\n" +
 
-                        "OPTIONAL MATCH (ms:Elements {MAIN_OBJ_NAME : row.MAIN_OBJ_NAME_SRC})\n" +
+                        "OPTIONAL MATCH (ms:Elements {MAIN_OBJ_NAME : row.MAIN_OBJ_NAME_SRC, MAIN_OBJ_TYPE: row.MAIN_OBJ_TYPE_SRC})\n" +
                         "WHERE ms.SUB_OBJ_NAME IS NULL AND row.SUB_OBJ_NAME_SRC IS NULL\n" +
-                        "OPTIONAL MATCH (ss:Elements {MAIN_OBJ_NAME : row.MAIN_OBJ_NAME_SRC, SUB_OBJ_NAME: row.SUB_OBJ_NAME_SRC})\n" +
+                        "OPTIONAL MATCH (ss:Elements {MAIN_OBJ_NAME : row.MAIN_OBJ_NAME_SRC, MAIN_OBJ_TYPE: row.MAIN_OBJ_TYPE_SRC, SUB_OBJ_NAME: row.SUB_OBJ_NAME_SRC, SUB_OBJ_TYPE: row.SUB_OBJ_TYPE_SRC})\n" +
                         "where ss.SUB_SUB_OBJ_NAME IS NULL AND row.SUB_SUB_OBJ_NAME_SRC IS NULL\n" +
-                        "OPTIONAL MATCH (sss:Elements {MAIN_OBJ_NAME : row.MAIN_OBJ_NAME_SRC, SUB_OBJ_NAME: row.SUB_OBJ_NAME_SRC, SUB_SUB_OBJ_NAME: row.SUB_SUB_OBJ_NAME_SRC})\n" +
+                        "OPTIONAL MATCH (sss:Elements {MAIN_OBJ_NAME : row.MAIN_OBJ_NAME_SRC, MAIN_OBJ_TYPE: row.MAIN_OBJ_TYPE_SRC, SUB_OBJ_NAME: row.SUB_OBJ_NAME_SRC, SUB_OBJ_TYPE: row.SUB_OBJ_TYPE_SRC, SUB_SUB_OBJ_NAME: row.SUB_SUB_OBJ_NAME_SRC, SUB_SUB_OBJ_TYPE: row.SUB_SUB_OBJ_TYPE_SRC})\n" +
                         "WHERE row.SUB_SUB_OBJ_NAME_SRC IS NOT NULL AND row.SUB_OBJ_NAME_SRC IS NOT NULL\n"+
 
-                        "UNWIND [ms,ss,sss] as src \n" +
+                        "UNWIND [ms,ss,sss] as src\n" +
                         "UNWIND [md,sd,ssd] as dst\n" +
                         "FOREACH (i in CASE WHEN src IS NOT NULL and dst IS NOT NULL THEN [1] ELSE [] END |\n" +
                         "CREATE (src)-[:"+SAPRelationLabels.REFERENCES +"]->(dst) )\n"
@@ -176,8 +176,49 @@ public class LoaderStep {
             System.out.println("Loader step was completed");
         }
 
-        // change/remove attribute names of all nodes
+        // add attributes to all nodes
+        //1. main elements
+        connector.executeWrite(
+                "MATCH(n:Elements)\n" +
+                        "WHERE n.SUB_OBJ_NAME IS NULL AND n.SUB_OBJ_TYPE IS NULL\n"+
+                        "SET n.obj_name = n.MAIN_OBJ_NAME, n.type = n.MAIN_OBJ_TYPE"
+        );
 
+        //2. sub elements
+        connector.executeWrite(
+                "MATCH(s:Elements)\n" +
+                        "WHERE s.SUB_OBJ_NAME IS NOT NULL AND s.SUB_SUB_OBJ_NAME IS NULL\n" +
+                        "SET s.object_name = s.SUB_OBJ_NAME , s.type = s.SUB_OBJ_TYPE"
+        );
+
+        //3. sub sub elements
+        connector.executeWrite(
+                "MATCH (ss)\n" +
+                        "WHERE ss.SUB_SUB_OBJ_NAME IS NOT NULL AND ss.SUB_SUB_OBJ_TYPE IS NOT NULL \n" +
+                        "SET ss.object_name = ss.SUB_SUB_OBJ_NAME , ss.type = ss.SUB_SUB_OBJ_TYPE\n"
+        );
+
+        //add type_name attribute
+        connector.executeWrite(
+                "MATCH(n:Elements)\n" +
+                        "SET n.type_name = CASE n.type\n" +
+                        "                    WHEN 'DEVC' THEN 'Namespace'\n" +
+                        "                    WHEN 'CLAS' THEN 'Class'\n" +
+                        "                    WHEN 'PROG' THEN 'Report'\n" +
+                        "                    WHEN 'INTF' THEN 'Interface'\n" +
+                        "                    WHEN 'FUGR' THEN 'FunctionGroup'\n" +
+                        "                    WHEN 'METH' THEN 'Method'\n" +
+                        "                    WHEN 'ATTR' THEN 'Attribute'\n" +
+                        "                    WHEN 'FUNC' THEN 'FunctionModule'\n" +
+                        "                    WHEN 'FORM' THEN 'FormRoutine'\n" +
+                        "                    WHEN 'VIEW' THEN 'View'\n" +
+                        "                    WHEN 'TABL' THEN 'Table'\n" +
+                        "                    WHEN 'STRU' THEN 'Struct'\n" +
+                        "                    WHEN 'DOMA' THEN 'Domain'\n" +
+                        "                    WHEN 'DTEL' THEN 'Dataelement'    \n" +
+                        "                    ELSE n.type\n" +
+                        "                    END"
+        );
     }
 
     private static class CSVInput {
