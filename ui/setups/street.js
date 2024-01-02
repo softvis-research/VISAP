e71df9fc6d64a697aeb97056afbd2629fba19371
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2024-01-02 18:03:54 +0100

    add street controller

---
+++ ui/setups/street.js
@@ -0,0 +1,48 @@
+const setup = {
+	controllers: [
+		{
+			name: "defaultLogger",
+
+			logActionConsole: false,
+			logEventConsole: false
+		},
+		{
+			name: "canvasHoverController",
+		},
+		{
+			name: "canvasSelectController",
+		},
+		{
+			name: "streetController",
+			sourceStartAtBorder: false,
+			targetEndAtBorder: false,
+			showInnerRelations: true,
+			curvedConnectors: true,
+
+			connectorColor: { r: 0, g: 0, b: 1 },
+		},
+	],
+
+	ui: {
+		name: "UI0",
+
+		area: {
+			name: "top",
+			orientation: "horizontal",
+			first: {},
+			second: {
+				size: "100%",
+				collapsible: false,
+				name: "canvas",
+				canvas: {},
+
+				controllers: [
+					{ name: "defaultLogger" },
+					{ name: "canvasHoverController" },
+					{ name: "canvasSelectController" },
+					{ name: "streetController" },
+				],
+			}
+		}
+	}
+};
