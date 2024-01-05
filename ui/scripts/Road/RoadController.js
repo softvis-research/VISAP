49098bf31ecf9d1d0e878d2384bece5fa48c0e18
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2024-01-05 16:04:20 +0100

    Update controller blueprint

--- ui/scripts/Road/RoadController.js
+++ ui/scripts/Road/RoadController.js
@@ -5,10 +5,37 @@ controllers.roadController = function () {
 		roadColorCalls: { r: 0, g: 0, b: 1 },
 		roadColorIsCalled: { r: 1, g: 0, b: 1 },
 		roadColorBidirectional: { r: 1, g: 0, b: 1 },
-		roadColorAmbiguous: { r: 0, g: 0, b: 0 },
+		roadColorAmbiguous: { r: 0, g: 1, b: 0 },
+
+		supportedEntityTypes: ["Class", "Report", "FunctionGroup", "Interface"]
     }
+
+	function initialize(setupConfig) {
+		application.transferConfigParams(setupConfig, controllerConfig);
+
+		// LD TODO: Add logic for guideMode helper initialization here (if we plan to implement multiple modes)
+
+		events.selected.on.subscribe(onEntitySelected);
+		events.selected.off.subscribe(onEntityDeselected);
+	}
+
+	function onEntitySelected(applicationEvent) {
+		const entityType = applicationEvent.entities[0].type;
+		if (controllerConfig.supportedEntityTypes.includes(entityType)) {
+			startElementId = [applicationEvent.entities[0]]
+			console.log(startElementId)
+			canvasManipulator.highlightEntities(startElementId, "green", { name: "roadController" });
+		  } else {
+		  }
+	}
+
+	function onEntityDeselected(applicationEvent) {
+		startElementId = [applicationEvent.entities[0]]
+		canvasManipulator.resetColorOfEntities(startElementId, { name: "roadController" })
+	}
 
 	// LD TODO: Implement me.
 	return {
+		initialize: initialize,
 	};
 }();
