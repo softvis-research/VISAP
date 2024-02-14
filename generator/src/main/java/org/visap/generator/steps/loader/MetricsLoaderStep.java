dc6293316a7b8fa0a47e6515a9db35fb389e5067
Louay <L.kurhabi@htwk.de>
2024-02-14 21:05:47 +0100

    update MetricsLoaderStep to handle the new CSV-Format. add creossrefs_Metrics.csv for Testing. rename Old version of Metrics-CSV to creossref_Metrics_Old.csv

--- generator/src/main/java/org/visap/generator/steps/loader/MetricsLoaderStep.java
+++ generator/src/main/java/org/visap/generator/steps/loader/MetricsLoaderStep.java
@@ -19,6 +19,7 @@ public class MetricsLoaderStep {
     private static final Log log = LogFactory.getLog(MetricsLoaderStep.class);
     private static final String FolderName = "Metrics";
     private static final String FileSuffix = "Metrics.csv";
+    private static final String[] Metrics = {"number_of_statements","number_of_object_references","number_of_exec_statements","maximum_nesting_depth","cyclomatic_complexity","keyword_named_variables","number_of_comments","halstead_difficulty","halstead_volume","halstead_effort","number_of_methods","number_of_interfaces","number_of_attributes","number_of_events","number_of_redefined_methods","number_of_protected_methods","number_of_public_methods","number_of_private_attributes","number_of_protected_attributes","number_of_public_attributes"};
 
     public static void main(String[] args) throws Exception {
 
@@ -37,8 +38,10 @@ public class MetricsLoaderStep {
 
         for (Path p : files) {
             log.info("Path to Metrics CSV: "+p);
-            createMetricNodes(p);
+           // createMetricNodes(p);
+            setAttributesFromCsv(p);
         }
+        /*
         List<Record> mainElementRecords = getMainElements();
         List<Record> subElementRecords = getSubElements();
         List<Record> subSubElementRecords = getSubSubElements();
@@ -46,10 +49,37 @@ public class MetricsLoaderStep {
         setAttributes(createHashMapFromRecordList(subElementRecords));
         setAttributes(createHashMapFromRecordList(subSubElementRecords));
         //clear Metric Nodes from Database
-        connector.executeWrite("MATCH (n:Metrics) DETACH DELETE n;");
+        connector.executeWrite("MATCH (n:Metrics) DETACH DELETE n;"); */
         userInput.close();
         //connector.close();
         log.info("MetricsLoader step was completed");
+    }
+
+    private static void setAttributesFromCsv(Path p) {
+        String pathToMetricsCsv = p.toString().replace("\\", "/");
+        pathToMetricsCsv = pathToMetricsCsv.replace(" ", "%20");
+        connector.executeWrite(
+                "LOAD CSV WITH HEADERS FROM \"file:///" + pathToMetricsCsv + "\"\n" +
+                        "AS row FIELDTERMINATOR ';' WITH row WHERE row.MAIN_OBJ_NAME IS NOT NULL\n" +
+                        "OPTIONAL MATCH (m:Elements {MAIN_OBJ_NAME : row.MAIN_OBJ_NAME, MAIN_OBJ_TYPE : row.MAIN_OBJ_TYPE})\n" +
+                        "WHERE m.SUB_OBJ_NAME IS NULL AND  row.SUB_OBJ_NAME IS NULL\n" +
+                        "OPTIONAL MATCH (s:Elements {MAIN_OBJ_NAME : row.MAIN_OBJ_NAME, MAIN_OBJ_TYPE : row.MAIN_OBJ_TYPE, SUB_OBJ_NAME: row.SUB_OBJ_NAME, SUB_OBJ_TYPE: row.SUB_OBJ_TYPE})\n" +
+                        "WHERE s.SUB_SUB_OBJ_NAME IS NULL AND row.SUB_SUB_OBJ_NAME IS NULL\n" +
+                        "OPTIONAL MATCH (ss:Elements {MAIN_OBJ_NAME : row.MAIN_OBJ_NAME, MAIN_OBJ_TYPE : row.MAIN_OBJ_TYPE, SUB_OBJ_NAME: row.SUB_OBJ_NAME, SUB_OBJ_TYPE: row.SUB_OBJ_TYPE, SUB_SUB_OBJ_NAME: row.SUB_SUB_OBJ_NAME, SUB_SUB_OBJ_TYPE: row.SUB_SUB_OBJ_TYPE})\n" +
+                        "WHERE row.SUB_SUB_OBJ_NAME IS NOT NULL AND row.SUB_OBJ_NAME IS NOT NULL\n"+
+                        "UNWIND [m,s,ss] as nod \n"+
+                        formatAttributes("nod","row")
+        );
+    }
+
+    private static String formatAttributes(String node, String metricsNode) {
+        StringBuilder resultAttributes = new StringBuilder("SET ");
+        for (String metric : Metrics){
+            String metricLine = node + "." + metric + " = " + metricsNode + "." + metric + " , ";
+            resultAttributes.append(metricLine);
+        }
+        resultAttributes.delete(resultAttributes.length() - 2 , resultAttributes.length());
+        return resultAttributes.toString();
     }
 
     private static void createMetricNodes(Path p) {
