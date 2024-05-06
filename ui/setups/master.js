71d70c9588ce9cc6f36d4c979509388744bec156
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2024-05-06 12:44:46 +0200

    include roadHighlight controller in master setup

--- ui/setups/master.js
+++ ui/setups/master.js
@@ -24,6 +24,24 @@ const setup = {
 
 			connectorColor: { r: 0, g: 0, b: 1 },
 		},
+		{
+			name: "roadController",
+			supportedEntityTypes: ["Class", "Report", "FunctionGroup", "Interface"],
+			roadHighlightVariant: "ParallelColorStripes",
+
+			colorsParallelColorStripes: {
+				calls: "lime",
+				isCalled: "magenta",
+			},
+
+			stripeProps : {
+				stripesOffset: 0.25,
+				posY: 0.75,
+				sphereRadius: 0.19999,
+				tubeRadius: 0.2,
+				shrinkPct: 0.7
+			}
+		}
 	],
 
 	ui: {
