353e3cc9fac8d475ff29182d1ef7d2bd5cee8ddf
Erik <haselbacherik@gmail.com>
2025-09-11 14:11:38 +0200

    ProcStepLoaderStep class completed - loads process step data into neo4j database

--- generator/src/main/java/org/visap/generator/steps/loader/ProcStepLoaderStep.java
+++ generator/src/main/java/org/visap/generator/steps/loader/ProcStepLoaderStep.java
@@ -15,6 +15,7 @@ public class ProcStepLoaderStep {
     private static final Log log = LogFactory.getLog(ProcStepLoaderStep.class);
     private static final String folderName = "ProcStep";
     private static final String fileSuffix = "ProcStep.csv";
+    private static final String labelName = "ProcStep";
 
     public static void main(String[] args) throws Exception {
         boolean isSilentMode = Config.setup.silentMode();
@@ -31,9 +32,43 @@ public class ProcStepLoaderStep {
             userInput.nextLine();
         }
 
-        // Verarbeitung der Daten
+        for (Path p : files){
+            log.info("Path to process step CSV: " + p);
+            log.info("creating process step nodes...");
+            createProcStepNodes(p);
+
+            log.info("assigning process step to element nodes...");
+            assignProcStep();
+
+            log.info("delete unnecessary process step nodes...");
+            connector.executeWrite("MATCH (n:" + labelName + ") DETACH DELETE n");
+        }
 
         userInput.close();
         log.info("ProcStepLoader step was completed");
+    }
+
+    private static void createProcStepNodes(Path p) {
+        String pathToProcStepCsv = p.toUri().toString();
+
+        connector.executeImplicit(
+                "LOAD CSV WITH HEADERS FROM \"" + pathToProcStepCsv + "\"\n" +
+                        "AS row FIELDTERMINATOR ';' WITH row WHERE row.MAIN_OBJ_NAME IS NOT NULL\n" +
+                        "CALL { WITH row \n" +
+                        "CREATE (n:" + labelName + ")\n" +
+                        "SET n = row," +
+                        "n.combinedKey = row.MAIN_OBJ_NAME + row.MAIN_OBJ_TYPE " +
+                        "+ COALESCE(row.SUB_OBJ_NAME, 'NONE') + COALESCE(row.SUB_OBJ_TYPE, 'NONE') " +
+                        "+ COALESCE(row.SUB_SUB_OBJ_NAME, 'NONE')  + COALESCE(row.SUB_SUB_OBJ_TYPE, 'NONE')" +
+                        "} IN TRANSACTIONS OF 10000 ROWS"
+        );
+    }
+
+    private static void assignProcStep(){
+        connector.executeWrite(
+                "MATCH (n:Elements), (m:" + labelName + ")\n"
+                        + "WHERE n.combinedKey = m.combinedKey\n"
+                        + "SET n.proc_step = m.PROC_STEP"
+        );
     }
 }
