3ec8c947c717af459d6fbb90bf85e5c521b3f6fc
Erik <haselbacherik@gmail.com>
2025-09-23 18:42:49 +0200

    distinction between railroadLane and railroadSleeper

--- generator/src/main/java/org/visap/generator/configuration/interfaces/Metropolis/Color.java
+++ generator/src/main/java/org/visap/generator/configuration/interfaces/Metropolis/Color.java
@@ -10,73 +10,76 @@ import org.visap.generator.configuration.Sources;
     Sources.CONFIG_PATH + "metropolis/Color.properties",
 })
 public interface Color extends Config {
-     @DefaultValue("#FF8C00")
-     String migrationElement();
+    @DefaultValue("#FF8C00")
+    String migrationElement();
 
-     @DefaultValue("#95A5A6")
-     String packageDistrict();
+    @DefaultValue("#95A5A6")
+    String packageDistrict();
 
-     @DefaultValue("#F4D93F")
-     String classDistrict();
+    @DefaultValue("#F4D93F")
+    String classDistrict();
 
-     @DefaultValue("#ffe7a3")
-     String localClassDistrict();
+    @DefaultValue("#ffe7a3")
+    String localClassDistrict();
 
-     @DefaultValue("#B80437")
-     String interfaceDistrict();
+    @DefaultValue("#B80437")
+    String interfaceDistrict();
 
-     @DefaultValue("#e79394")
-     String localInterfaceDistrict();
+    @DefaultValue("#e79394")
+    String localInterfaceDistrict();
 
-     @DefaultValue("#85C1E9")
-     String reportDistrict();
+    @DefaultValue("#85C1E9")
+    String reportDistrict();
 
-     @DefaultValue("#7D3C98")
-     String functionGroupDistrict();
+    @DefaultValue("#7D3C98")
+    String functionGroupDistrict();
 
-     @DefaultValue("#C5CEA9")
-     String defaultDistrictValue();
-
-     @DefaultValue("#000000")
-     String attributeBuilding();
-
-     @DefaultValue("#b80437")
-     String interfaceBuilding();
-
-     @DefaultValue("#FFFFFF")
-     String methodBuilding();
-
-     @DefaultValue("#335b9C")
-     String reportBuilding();
-
-     @DefaultValue("#FFFFFF")
-     String formRoutineBuilding();
-
-     @DefaultValue("#FFFFFF")
-     String functionModuleBuilding();
-
-     @DefaultValue("#FFFFFF")
-     String defaultBuildingValue();
-     
-     @DefaultValue("#000000")
-     String road();
+    @DefaultValue("#C5CEA9")
+    String defaultDistrictValue();
 
     @DefaultValue("#000000")
-    String railroad();
+    String attributeBuilding();
 
-     // DDIC
-     @DefaultValue("#229954")
-     String ddicDistrict();
-     @DefaultValue("#1A5276")
-     String tableDistrict();
-     @DefaultValue("#FCFAFA")
-     String tableBuilding();
-     @DefaultValue("#8AB8E6")
-     String viewBuilding();
-     @DefaultValue("#0E573B")
-     String domainBuilding();
-     @DefaultValue("#FCFAFA")
-     String structBuilding();
-     @DefaultValue("#1E8449")
-     String dataelementBuilding();
+    @DefaultValue("#b80437")
+    String interfaceBuilding();
+
+    @DefaultValue("#FFFFFF")
+    String methodBuilding();
+
+    @DefaultValue("#335b9C")
+    String reportBuilding();
+
+    @DefaultValue("#FFFFFF")
+    String formRoutineBuilding();
+
+    @DefaultValue("#FFFFFF")
+    String functionModuleBuilding();
+
+    @DefaultValue("#FFFFFF")
+    String defaultBuildingValue();
+     
+    @DefaultValue("#000000")
+    String road();
+
+    @DefaultValue("#B7410E")
+    String railroadLane();
+
+    @DefaultValue("#808076")
+    String railroadSleeper();
+
+    // DDIC
+    @DefaultValue("#229954")
+    String ddicDistrict();
+    @DefaultValue("#1A5276")
+    String tableDistrict();
+    @DefaultValue("#FCFAFA")
+    String tableBuilding();
+    @DefaultValue("#8AB8E6")
+    String viewBuilding();
+    @DefaultValue("#0E573B")
+    String domainBuilding();
+    @DefaultValue("#FCFAFA")
+    String structBuilding();
+    @DefaultValue("#1E8449")
+    String dataelementBuilding();
 }
